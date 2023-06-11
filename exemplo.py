f26417e586472b260a6940ee9b1f9a906c864ee5
devglord <maximianobatista1@gmail.com>
2023-06-11 19:56:08 +0100

    m

--- exemplo.py
+++ exemplo.py
@@ -6,7 +6,6 @@ class Pessoa:
     def __init__(self,nome,idade):
         self.nome = nome
         self.idade = idade
-
 person = Pessoa('Mateus',19)
 people = Pessoa('Mario',21)
 
@@ -15,3 +14,5 @@ conjunto = (vars(person),vars(people))
 with open(LOCAL_ARQUIVO,'w',encoding='utf8') as arquivo:
     json.dump(conjunto,arquivo,ensure_ascii=False,indent=2)
 
+with open(LOCAL_ARQUIVO,'r') as arquivo:
+    print(json.load(arquivo))
