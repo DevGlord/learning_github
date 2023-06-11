7d6051d287040b124d1f8d5a96a71fa7f760c19c
devglord <maximianobatista1@gmail.com>
2023-06-11 19:17:42 +0100

    Criando um exemplo no programa python

---
+++ exemplo.py
@@ -0,0 +1,17 @@
+import json
+
+LOCAL_ARQUIVO = 'ex1.json'
+
+class Pessoa:
+    def __init__(self,nome,idade):
+        self.nome = nome
+        self.idade = idade
+
+person = Pessoa('Mateus',19)
+people = Pessoa('Mario',21)
+
+conjunto = (vars(person),vars(people))
+
+with open(LOCAL_ARQUIVO,'w',encoding='utf8') as arquivo:
+    json.dump(conjunto,arquivo,ensure_ascii=False,indent=2)
+
