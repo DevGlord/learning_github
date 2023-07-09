2dd9d68c87c6d0b6319217d8980f44f81fada8b1
devglord <maximianobatista1@gmail.com>
2023-07-09 20:16:43 +0100

    Primeiro exemplo resolvido

--- escola.py
+++ escola.py
@@ -1,3 +1,4 @@
+import json
 class Escola:
     def __init__(self,nome_aluno,idade_aluno):
         self.nome_aluno = nome_aluno
@@ -6,7 +7,7 @@ class Escola:
     def apresentacao_alunos(self):
         print(f'O {self.nome_aluno} tem {self.idade_aluno}')
 
-n1 = str(input('Nome do aluno:'))
-n2 = int(input('Idade do aluno:'))
-c1 = Escola(n1,n2)
+
+c1 = Escola('Maximiano',19)
+
 c1.apresentacao_alunos()
